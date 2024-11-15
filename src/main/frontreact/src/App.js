56f1dbdbb031c48c35ef4b66eccdb4db84f0020d
rwanda95do <dlsdnr2379@naver.com>
2024-11-15 17:30:15 +0900

    3

--- src/main/frontreact/src/App.js
+++ src/main/frontreact/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Spring Boot + react + NCP</h1>
-      <h3>카운트 : 2</h3>
+      <h3>카운트 : 3</h3>
       <img
         src="./image/r.png"
         alt="현재위치는 index.html이 있는 public입니다."
