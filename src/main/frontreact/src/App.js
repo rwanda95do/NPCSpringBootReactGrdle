2cb5b338810a722dbbc8c0313f1621d649625ed8
rwanda95do <dlsdnr2379@naver.com>
2024-11-15 14:35:25 +0900

    1

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spring Boot + react + NCP</h1>
+      <h3>카운트 : 1</h3>
+      <img
+        src="./image/r.png"
+        alt="현재위치는 index.html이 있는 public입니다."
+      ></img>
+    </div>
+  );
+}
+
+export default App;
